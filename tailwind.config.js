e533094e47f89d86ef11c4d0d14d01bbfa439a37
Henry Okeke <iamhenryokeke@gmail.com>
2023-03-11 22:12:18 +0100

    added config for colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        body:['Inter']
+      },colors: {
+        blue: {
+          950: 'hsl(240, 100%, 5%)',
+          1000: '#8E8FE0',
+        },
+        softOrange : "hsl(35, 77%, 62%)",
+        softRed : "hsl(5, 85%, 63%)"
+      }, gridTemplateColumns : {
+        "16":"0.6fr 1fr",
+        "18" : "1fr 0.4fr"
+      }
+    },
   },
   plugins: [],
 }
